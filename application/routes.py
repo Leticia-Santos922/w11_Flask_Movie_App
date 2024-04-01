cc51bf5c571cb23964626426fe49dab242675cbb
leticia <leticia.nuness.santos@gmail.com>
2024-04-01 11:48:55 +0100

    added all files - linked to css file successfully

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template
-
+from flask import render_template, request, url_for
+import random
 from application import app
 
 
@@ -9,6 +9,37 @@ def home():
     return render_template('home.html', title='Home')
 
 
-@app.route('/welcome/<name>')
-def welcome(name):
-    return render_template('welcome.html', name=name, group='Everyone')
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About', favourites=['crocheting', 'reading'])
+
+
+movies = [
+    {"title": "The Shawshank Redemption", "genre": "Drama", "year": 1994},
+    {"title": "The Godfather", "genre": "Crime", "year": 1972},
+    {"title": "The Dark Knight", "genre": "Action", "year": 2008},
+    {"title": "Pulp Fiction", "genre": "Crime", "year": 1994},
+    {"title": "Forrest Gump", "genre": "Drama", "year": 1994}
+    # Add more movies as needed
+]
+
+
+@app.route('/movie', methods=['GET', 'POST'])
+def movie():
+    # pick a random movie
+    if request.method == 'POST':
+        # Pick a random movie
+        movie = random.choice(movies)
+        return render_template('movie.html', title='Movie', movies=movies, movie=movie)
+    else:
+        return render_template('movie.html', title='Movie', movies=movies)
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Register')
