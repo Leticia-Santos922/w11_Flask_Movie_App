3a5e8ad62a6d722b3edcedd50982522f51ec64fb
leticia <leticia.nuness.santos@gmail.com>
2024-04-29 22:43:15 +0100

    tidy layout

--- data_access.py
+++ data_access.py
@@ -16,29 +16,19 @@ def get_random_movie(genre):
     # established a connection to the database
     moviedb = get_database_connection()
 
+    # more efficient code
     # mysql cursor is instantiating the mysql class
     cursor = moviedb.cursor()
-    # Execute query to fetch all movies from the database
-    # Construct the SQL query
+
     if genre:
-        query = "SELECT * FROM movies WHERE genre = %s"
-        # execute is passing through the query above: the %s is the parameters and placeholder to pass through any values in
-        # %s which is the genres "Animation, Drama"
-        # genre is a tuple because it does not change and same placeholder
+        query = "SELECT * FROM movies WHERE genre = %s ORDER BY RAND() LIMIT 1"
         cursor.execute(query, (genre,))
     else:
-        # anything that is not a genre executes this query
-        cursor.execute("SELECT * FROM movies")
+        query = "SELECT * FROM movies ORDER BY RAND() LIMIT 1"
+        cursor.execute(query)
 
-    # Fetch all rows from the result set
-    movies = cursor.fetchall()
-    # todo: is it good practice to close the connection and what does this mean?
-    # Close cursor and connection
-    # cursor.close()
-    # moviedb.close()
-    # Pick a random movie from the list
-    if movies:
-        random_movie = random.sample(movies, 1)[0]  # Select a single random movie from the list
-        return random_movie
-    else:
-        return None
+        # Fetch one result
+    random_movie = cursor.fetchone()
+
+    return random_movie
+
